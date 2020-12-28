4ceb4cf9502c5b0375a519db1267bee995154985
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-12-28 06:35:36 +0300

    47

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 
-let rerenderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
@@ -17,14 +16,12 @@ let rerenderEntireTree = () => {
     </React.StrictMode>,
     document.getElementById('root'),
   );
-};
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-rerenderEntireTree();
-store.subscribe(() => {
-  rerenderEntireTree();
-});
+
+
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-
 let initialState = {
   posts: [
     {
@@ -20,11 +19,11 @@ let initialState = {
     },
   ],
   newPostText: '',
-}
+};
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD-POST':
+    case 'ADD-POST': {
       let newPost = {
         text: state.newPostText,
         id: 1,
@@ -32,23 +31,24 @@ const profileReducer = (state = initialState, action) => {
         img:
           'https://i.pinimg.com/originals/80/e5/0d/80e50d775e936217f89af2de58ba7646.jpg',
       };
-      state.posts.push(newPost);
-      state.newPostText = '';
-
-      return state;
-
-    case 'UPDATE-NEW-POST-TEXT':
-      state.newPostText = action.newText;
-
-      return state;
-
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(newPost);
+      stateCopy.newPostText = '';
+      return stateCopy;
+    }
+    case 'UPDATE-NEW-POST-TEXT': {
+      let stateCopy = { ...state };
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
     default: {
       return state;
     }
   }
 };
 export const updateNewPostTextActionCreater = (text) => {
- return { type: UPDATE_NEW_POST_TEXT, newText: text };
+  return { type: UPDATE_NEW_POST_TEXT, newText: text };
 };
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export default profileReducer;
