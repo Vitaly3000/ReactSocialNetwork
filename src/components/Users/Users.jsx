8ebfef594a49f8f474bc4529bd5980ee36e38993
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-12-29 08:37:21 +0300

    53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,38 +3,39 @@ import style from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/img/user.jpg';
 
-const Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       .then((responce) => {
-        props.setUsers(responce.data.items);
+        this.props.setUsers(responce.data.items);
       });
   }
+  render() {
+    return (
+      <div className={style.users}>
+        {this.props.users.map((u) => {
+          return (
+            <div className={style.user}>
+              <img
+                className={style.photo}
+                src={u.photo ? u.small : userPhoto}
+                alt="a sad frog"
+              />
+              <div className={style.name}>{u.name}</div>
 
-  return (
-    <div className={style.users}>
-      {props.users.map((u) => {
-        return (
-          <div className={style.user}>
-            <img
-              className={style.photo}
-              src={u.photo ? u.small : userPhoto}
-              alt="a sad frog"
-            />
-            <div className={style.name}>{u.name}</div>
-
-            <button
-              onClick={() => {
-                props.toggleFollow(u.id);
-              }}>
-              {u.followed ? 'Отписаться' : 'Подписаться'}
-            </button>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
+              <button
+                onClick={() => {
+                  this.props.toggleFollow(u.id);
+                }}>
+                {u.followed ? 'Отписаться' : 'Подписаться'}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 export default Users;
