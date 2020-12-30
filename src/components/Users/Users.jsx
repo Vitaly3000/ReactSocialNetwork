e954b5e84cd6f714a365cce55e70027375f0958f
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-12-30 09:10:15 +0300

    56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,75 +1,46 @@
 import React from 'react';
 import style from './Users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../assets/img/user.jpg';
-
-class Users extends React.Component {
-  componentDidMount() {
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page${this.props.currentPage}&count=${this.props.pageSize}`,
-      )
-      .then((responce) => {
-        this.props.setUsers(responce.data.items);
-        if (responce.data.totalCount > 500) {
-          this.props.setTotalUsersCount(200);
-        }
-      });
+const Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-      )
-      .then((responce) => {
-        this.props.setUsers(responce.data.items);
-      });
-  };
-  render() {
-    let pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize,
-    );
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
-      <div className={style.users}>
-        <div className={style.paginator}>
-          {pages.map((p) => {
-            return (
-              <button
-                onClick={() => this.onPageChanged(p)}
-                className={this.props.currentPage === p && style.currentPage}>
-                {p}
-              </button>
-            );
-          })}
-        </div>
-        {this.props.users.map((u) => {
+  return (
+    <div className={style.users}>
+      <div className={style.paginator}>
+        {pages.map((p) => {
           return (
-            <div className={style.user}>
-              <img
-                className={style.photo}
-                src={u.photo ? u.small : userPhoto}
-                alt="a sad frog"
-              />
-              <div>Name:</div>
-              <div className={style.name}>{u.name}</div>
-
-              <button
-                onClick={() => {
-                  this.props.toggleFollow(u.id);
-                }}>
-                {u.followed ? 'Отписаться' : 'Подписаться'}
-              </button>
-            </div>
+            <button
+              onClick={() => props.onPageChanged(p)}
+              className={props.currentPage === p && style.currentPage}>
+              {p}
+            </button>
           );
         })}
       </div>
-    );
-  }
-}
+      {props.users.map((u) => {
+        return (
+          <div className={style.user}>
+            <img
+              className={style.photo}
+              src={u.photo ? u.small : userPhoto}
+              alt="a sad frog"
+            />
+            <div>Name:</div>
+            <div className={style.name}>{u.name}</div>
+
+            <button
+              onClick={() => {
+                props.toggleFollow(u.id);
+              }}>
+              {u.followed ? 'Отписаться' : 'Подписаться'}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,3 +1,8 @@
+import Users from './Users';
+import React from 'react';
+
+import * as axios from 'axios';
+
 import { connect } from 'react-redux';
 import {
   setCurrentPageActionCreator,
@@ -5,7 +10,43 @@ import {
   setUsersActionCreator,
   toggleFollowActionCreator,
 } from '../../redux/users-reducer';
-import Users from './Users';
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page${this.props.currentPage}&count=${this.props.pageSize}`,
+      )
+      .then((responce) => {
+        this.props.setUsers(responce.data.items);
+        if (responce.data.totalCount > 500) {
+          this.props.setTotalUsersCount(200);
+        }
+      });
+  }
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+      )
+      .then((responce) => {
+        this.props.setUsers(responce.data.items);
+      });
+  };
+  render() {
+    return (
+      <Users
+        users={this.props.users}
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        toggleFollow={this.props.toggleFollow}
+      />
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
   return {
@@ -29,8 +70,6 @@ let mapDispatchToProps = (dispatch) => {
     setTotalUsersCount: (totalUsersCount) => {
       dispatch(setTotalUsersCountActionCreator(totalUsersCount));
     },
-    
   };
 };
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
