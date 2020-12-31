d7109cda12a732698fa2842139f1e7bdb53721e1
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-12-31 10:11:27 +0300

    61

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore } from 'redux';
+import authReducer from './auth-reducer';
 import dialogsReducer from './dialogs-reducer';
 import profileReducer from './profile-reducer';
 import usersReducer from './users-reducer';
@@ -7,7 +8,9 @@ let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
 
 let store = createStore(reducers);
+Window.store = store;
 export default store;
