9dc0ea1b27e620fd77229f6eb79d8a5fcb46dacc
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2021-01-06 15:08:52 +0300

    78

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
-import { getAuthUserData } from '../../redux/auth-reducer';
+import { getAuthUserData, logout } from '../../redux/auth-reducer';
 import Header from './Header';
 
 class HeaderContainer extends React.Component {
@@ -19,4 +18,6 @@ const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
 });
 
-export default connect(mapStateToProps, { getAuthUserData })(HeaderContainer);
+export default connect(mapStateToProps, { getAuthUserData, logout })(
+  HeaderContainer,
+);
