c94c097a78995a6349524df3131629f3cb079dc9
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2021-01-16 22:12:19 +0300

    96

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,11 @@ import style from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/img/user.jpg';
 import ProfileStatus from './ProfileStatus';
 const ProfileInfo = (props) => {
+  const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+      props.savePhoto(e.target.files[0]);
+    }
+  };
   return (
     <div>
       {props.profile && (
@@ -12,6 +17,9 @@ const ProfileInfo = (props) => {
             src={props.profile.photos.large || userPhoto}
             alt=""
           />
+          {props.isOwner && (
+            <input type="file" onChange={onMainPhotoSelected} />
+          )}
           <div>
             <span>{props.status || 'Статуса нету'}</span>
           </div>
